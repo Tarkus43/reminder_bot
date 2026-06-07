7b3ddb6eade5b6a1d1a89dc8f4086dcec7fcb28f
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-06-07 19:53:36 +0200

    i forgor about camel case :/

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Telegraf } from "telegraf";
 import { config } from "./config.js";
 const bot = new Telegraf(config.telegramToken);
-import { mainMenuMarkup } from "./markups/main_menu_markup.js";
+import { mainMenuMarkup } from "./markups/mainMenuMarkup.js";
 
 bot.start((ctx) => {
   ctx.reply("Welcome to my reminder/timer bot! Choose an option below:", mainMenuMarkup)
