6032afa3c1ceffd9819c9791e204f5b91f2467a5
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-06-11 11:13:21 +0200

    added chalk to project and connected to databse

---
+++ connect.js
@@ -0,0 +1,26 @@
+// db.js
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+import chalk from 'chalk'
+
+export let db = null
+
+try {
+    db = await open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    })
+
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS reminders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        message TEXT NOT NULL,
+        description TEXT,
+        remind_at INTEGER NOT NULL,
+        is_fired INTEGER DEFAULT 0
+        )
+    `)
+} catch (error) {
+  console.log(chalk.red('error creating table or connecting to database:', error))
+}
