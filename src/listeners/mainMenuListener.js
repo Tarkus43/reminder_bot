af8a0d3488f56ed0f05b20097410f4845155498c
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-07-10 09:33:44 +0200

    added parsing for user time input

--- src/listeners/mainMenuListener.js
+++ src/listeners/mainMenuListener.js
@@ -1,14 +1,28 @@
 import { backToMainMenuMarkup } from "../markups/backToMainMenuMarkup.js";
+import * as chrono from "chrono-node";
+
+
 export const mainMenuListener = (bot) => {
   bot.hears('⏰ My timers', (ctx) => {
-    ctx.reply('Here are your timers:', backToMainMenuMarkup);
-  });
-  
-  bot.hears('➕ New timer', (ctx) => {
-    ctx.reply('function in progress :)', backToMainMenuMarkup);
-  });
-  
+    ctx.reply('Here are your timers:', backToMainMenuMarkup);      
+    });
+    
   bot.hears('❓ Help', (ctx) => {
-    ctx.reply('Help:', backToMainMenuMarkup);
-  });
+    ctx.reply('Help:', backToMainMenuMarkup) ;
+    });
+
+  bot.hears('➕ New timer', (ctx) => {
+    ctx.reply('enter in EN any time you want', backToMainMenuMarkup)
+    bot.on('text', (ctx) => {
+      const result = ctx.message.text 
+      const time = chrono.parse(result, new Date(), {forwardDate: true,})
+      const msTime = time[0].date().getTime()
+      if (!msTime) {
+        ctx.reply('please, try again, I didnt understand', backToMainMenuMarkup)
+      } else {
+        ctx.reply('your time is: ' + msTime, backToMainMenuMarkup)
+      }
+      // usersStates.delete(ctx.from.id)
+    })
+  })
 }
